f638476655a3ab552c0821e903cefda177485e94
960761 <275332466@qq.com>
2018-06-06 20:23:22 +0800

    Create FlyAsRocket.java

---
+++ code/HeadFirst_DesignPattern/ch01_StrategyPattern/src/FlyAsRocket.java
@@ -0,0 +1,10 @@
+//Author:xin
+//Date: 2018-06-06
+
+//Strategy Pattern Demo
+
+public class FlyAsRocket implements FlyBehavior {
+	public void fly(){
+		System.out.println(&quot;fly like a Rocket&quot;);
+	}
+}
