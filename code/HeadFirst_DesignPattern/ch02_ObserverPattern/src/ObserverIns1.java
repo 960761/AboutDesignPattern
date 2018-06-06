fca6825cf978fa366343b1d6bedcab58870d4678
960761 <275332466@qq.com>
2018-06-06 20:47:14 +0800

    Update ObserverIns1.java

--- code/HeadFirst_DesignPattern/ch02_ObserverPattern/src/ObserverIns1.java
+++ code/HeadFirst_DesignPattern/ch02_ObserverPattern/src/ObserverIns1.java
@@ -1,1 +1,21 @@
-
+public class ObserverIns1 implements Observer, Display {
+	private float temp;
+	private float humi;
+	private float pre;
+	private Subject weatherData;
+	
+	public ObserverIns1(Subject weatherData){
+		this.weatherData = weatherData;
+		weatherData.registerObserver(this);
+	}
+	
+	public void update(float temp, float humi, float pre){
+		this.temp = temp;
+		this.humi = humi;
+		this.pre = pre;
+		display();
+	}
+	public void display(){
+		System.out.println(&quot;Current Conditions: &quot; + temp + &quot;F degrees and &quot; + humi + &quot;% humidity and &quot; + pre + &quot; pressue.&quot;);
+	}
+}
