c6bab01e2f2790d46a78fab2350fcabaaff0f556
960761 <275332466@qq.com>
2018-06-06 20:51:55 +0800

    Update WeatherData2.java

--- code/HeadFirst_DesignPattern/ch02_ObserverPattern/src/WeatherData2.java
+++ code/HeadFirst_DesignPattern/ch02_ObserverPattern/src/WeatherData2.java
@@ -1,1 +1,25 @@
+//JAVA built-in observer pattern
 
+import java.util.Observable;
+import java.util.Observer;
+
+public class WeatherData2 extends Observable {
+	private float temp;
+	private float humi;
+	private float pres;
+	
+	public void measurementsChanged(){
+		setChanged();
+		notifyObservers();
+	}
+	public void setMeasurements(float temp, float hum, float pre){
+		this.temp = temp;
+		this.humi = hum;
+		this.pres = pre;
+		
+		measurementsChanged();
+	}
+	public float getTemp(){return temp;}
+	public float getHumi(){return humi;}
+	public float getPres(){return pres;}
+}
