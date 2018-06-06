acccb13fbb2c35158cb11baad866adc839436b17
960761 <275332466@qq.com>
2018-06-06 20:43:46 +0800

    Update Subject.java

--- code/HeadFirst_DesignPattern/ch02_ObserverPattern/src/Subject.java
+++ code/HeadFirst_DesignPattern/ch02_ObserverPattern/src/Subject.java
@@ -1,1 +1,10 @@
+//author:xin
+//date:2018-6-6
 
+//self-defined observer pattern
+
+public interface Subject {
+	public void registerObserver(Observer o);
+	public void removeObserver(Observer o);
+	public void notifyObservers();
+}
