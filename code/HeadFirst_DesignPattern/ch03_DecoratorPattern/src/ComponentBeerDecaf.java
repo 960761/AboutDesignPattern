8237e3f2d499fc085e58220d79f3baa9984afa10
960761 <275332466@qq.com>
2018-06-11 20:18:03 +0800

    Create ComponentBeerDecaf.java

---
+++ code/HeadFirst_DesignPattern/ch03_DecoratorPattern/src/ComponentBeerDecaf.java
@@ -0,0 +1,7 @@
+//concrete component class
+
+public class ComponentBeerDecaf extends ComponentBeer {
+	public ComponentBeerDecaf(){ desc = &quot;Decaf&quot;;}
+	
+	public double cost(){ return 1.49 ;}
+}
