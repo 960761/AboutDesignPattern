9dc644c95523e8002903bd839d496c5fef0acee9
960761 <275332466@qq.com>
2018-06-11 20:17:22 +0800

    Create ComponentBeer.java

---
+++ code/HeadFirst_DesignPattern/ch03_DecoratorPattern/src/ComponentBeer.java
@@ -0,0 +1,10 @@
+//component abstract class
+
+public abstract class ComponentBeer {
+	String desc = &quot;Unknown beer&quot;;
+	
+	public String getDesc(){
+		return desc;
+	}
+	public abstract double cost();
+}
