f1061676d324d25b35f6625cab9d80095bbb6ecb
960761 <275332466@qq.com>
2018-06-12 21:40:28 +0800

    Create Tea.java

---
+++ code/HeadFirst_DesignPattern/ch08_TemplateMethodPattern/src/Tea.java
@@ -0,0 +1,9 @@
+public class Tea extends CoffeeAndTea {
+	public void brew(){
+		System.out.println(&quot;Dripping Tea through filter&quot;);
+	}
+	
+	public void addCondiments(){
+		System.out.println(&quot;Adding lemon&quot;);
+	}
+}
